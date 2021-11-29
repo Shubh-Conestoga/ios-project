8360961ccb05ef39020e908fa1194488d307bf33
Shubh-Conestoga <Spatel8035@conestogac.on.ca>
2021-11-29 13:23:44 -0500

    First Commit

---
+++ WeatherApp/WeatherApp/api/apis.swift
@@ -0,0 +1,13 @@
+//
+//  apis.swift
+//  WeatherApp
+//
+//  Created by user193659 on 11/29/21.
+//
+
+import Foundation
+
+let WEATHER_BASE_API = "https://api.openweathermap.org/data/2.5/weather?"
+let WEATHER_ICON_API = "https://openweathermap.org/img/w/"
+
+
